488659a2b66c12eaec60180c748e18d961460e3f
raysonzheng10 <raysonzheng10@gmail.com>
2023-06-18 14:53:25 -0400

    changed colors of the board, tested adding in pieces, added pngs for chess pieces

---
+++ helper/constants.py
@@ -0,0 +1,4 @@
+# Defining constant variables that will not be changed
+
+# Width and Height for the general window
+WIDTH, HEIGHT = 800, 800
--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import pygame
 from sys import exit
+from helper.constants import WIDTH, HEIGHT
 
-WIDTH, HEIGHT = 800, 800
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Chess')
 clock = pygame.time.Clock()
+
+green_surface = pygame.Surface((100,100))
+green_surface.fill((127, 166, 80))
+
+white_surface = pygame.Surface((100,100))
+white_surface.fill((238, 238, 210))
+
+pawn_surface = pygame.image.load('white_pawn.png')
+pawn_surface = pygame.transform.scale(pawn_surface, (100,100))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
+    for i in range(8):
+        for j in range(8):
+            if (i + j) % 2 == 1:
+                screen.blit(green_surface, (100 * i, 100 * j))
+            else:
+                screen.blit(white_surface, (100 * i, 100 * j))
+    screen.blit(pawn_surface, (100,100))
     pygame.display.update()
     clock.tick(60)
