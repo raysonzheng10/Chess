485e27e8d1ba2d87e056c1749f2a987956f85863
raysonzheng10 <raysonzheng10@gmail.com>
2023-06-19 14:54:01 -0400

    cleaned up main.py and main(), pulled functions out

--- main.py
+++ main.py
@@ -17,26 +17,31 @@ clock = pygame.time.Clock()
 #CREATE BOARD
 board = Board()
 
+# Function that puts the pos of mouse click into board.selected_tile
+def retrieve_mouse_pos(event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        # Gets the row and col positions from the click
+        mouse_pos = pygame.mouse.get_pos()
+        row = mouse_pos[1] // 100
+        col = mouse_pos[0] // 100
+        board.selected_tile = [row, col]
 
+# Function that handles exiting the game
+def game_quit(event):
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        exit()
+
+    
 def main():
     while True:
         # Main loop, listening to different events/inputs
         for event in pygame.event.get():
-            # User clicks on the board
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                # Gets the row and col positions from the click
-                mouse_pos = pygame.mouse.get_pos()
-                row = mouse_pos[1] // 100
-                col = mouse_pos[0] // 100
-                board.selected_tile = [row, col]
+            # Puts the pos of mouse click into board.selected_tile
+            retrieve_mouse_pos(event)
 
-                print(board.selected_tile)
-                # Check and see what was clicked on
-                
             # Quit functionality
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+            game_quit(event)
             
             #if event type == start game, run initlize
         
